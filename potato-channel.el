783f218f76a06d7692443c89d4ab283e3faaac3d
Elias Martenson <lokedhs@gmail.com>
2015-08-03 19:15:27 +0800

    Improvements to the new channel mode

--- potato-channel.el
+++ potato-channel.el
@@ -11,6 +11,15 @@
     (define-key map (kbd "RET") 'potato-send-input-line)
     map))
 
+(defun potato--insert-message (message-id timestamp from text)
+  (save-excursion
+    (goto-char potato--output-marker)
+    (let ((inhibit-read-only t))
+      (insert (propertize (format "[%s - %s] %s\n" timestamp from text)
+                          'read-only t
+                          'potato-message-id message-id
+                          'potato-timestamp timestamp)))))
+
 (define-derived-mode potato-channel-mode nil "Potato"
   "Mode for Potato channel content"
   (use-local-map potato-channel-mode-map)
@@ -18,6 +27,13 @@
   (setq-local potato--input-marker (make-marker))
   (set-marker potato--output-marker (point-max))
   (insert "channel> ")
+  (add-text-properties (point-min) (point-max) '(read-only t rear-nonsticky t front-sticky (read-only)))
+  (set-marker-insertion-type potato--output-marker t)
   (set-marker potato--input-marker (point-max)))
+
+(cl-defun debug-insert-message (&optional (time 0))
+  (let ((timestamp (format "20150101T12:00:%02dZ" time))
+        (message-id (format "msgid%d" time)))
+    (potato--insert-message message-id timestamp "Palle" "Hello test message here")))
 
 (provide 'potato-channel)
