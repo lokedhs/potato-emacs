43a3b1dfbcd42aaba2e387334a65b04faff37db7
Elias Martenson <lokedhs@gmail.com>
2015-12-18 15:44:00 +0800

    Channel selector, improved rendering and other fixes

--- potato.el
+++ potato.el
@@ -1,7 +1,6 @@
 ;;; -*- lexical-binding: t -*-
 
 (require 'url)
-(require 'potato-channel)
 
 (defgroup potato nil
   "Potato client implementation for Emacs"
@@ -74,6 +73,13 @@
       (error "No value for tag: %s" tag))
     (cdr value)))
 
+(defun potato--json-parse-result-buffer ()
+  (goto-char (point-min))
+  (search-forward "\n\n")
+  (let* ((content (buffer-substring (point) (point-max)))
+         (decoded-content (decode-coding-string content 'utf-8)))
+    (json-read-from-string decoded-content)))
+
 (defun potato--url-handler (status buffer callback)
   (let ((error-status (getf status :error)))
     (if error-status
@@ -81,26 +87,147 @@
           (message "Got error: %S" status)
           (signal (car error-status) (cdr error-status)))
       ;; ELSE: No error
-      (progn
-        (goto-char (point-min))
-        (search-forward "\n\n")
-        (let* ((content (buffer-substring (point) (point-max)))
-               (decoded-content (decode-coding-string content 'utf-8))
-               (data (json-read-from-string decoded-content)))
-          (with-current-buffer buffer
-            (funcall callback data)))))))
+      (let ((data (potato--json-parse-result-buffer)))
+        (with-current-buffer buffer
+          (funcall callback data))
+        (let ((kill-buffer-query-functions nil))
+          (kill-buffer (current-buffer)))))))
+
+(cl-defmacro with-url-params ((url-sym url method use-global) &body body)
+  (declare (indent 1))
+  (let ((use-global-sym (gensym "use-global-"))
+        (base-sym (gensym "base-")))
+    `(let* ((,use-global-sym ,use-global)
+            (,base-sym (if ,use-global-sym potato-url potato--active-url)))
+       (let ((url-request-method ,method)
+             (url-request-extra-headers (list (cons "API-Token" 
+                                                    (if ,use-global-sym
+                                                        potato-api-token
+                                                      potato--active-api-token))))
+             (,url-sym (format "%s%sapi/1.0%s"
+                               ,base-sym
+                               (if (eql (aref ,base-sym (1- (length ,base-sym))) ?/) "" "/")
+                               ,url)))
+         ,@body))))
 
 (defun potato--url-retrieve (url method callback)
-  (let ((url-request-method method)
-        (url-request-extra-headers `(("API-Token" . ,potato--active-api-token)))
-        (url (format "%s%sapi/1.0%s"
-                     potato--active-url
-                     (if (eql (aref potato--active-url (1- (length potato--active-url))) ?/) "" "/")
-                     url)))
-    (let ((buffer (current-buffer)))
-      (url-retrieve url (lambda (status)
-                          (potato--url-handler status buffer callback))
+  (let ((buffer (current-buffer)))
+    (with-url-params (result-url url method nil)
+      (url-retrieve result-url (lambda (status)
+                                 (message "The content of variable buffer now: %S" buffer)
+                                 (potato--url-handler status buffer callback))
                     nil t))))
+
+(cl-defun potato--url-retrieve-synchronous (url method &key use-global)
+  (with-url-params (result-url url method use-global)
+    (let ((buffer (url-retrieve-synchronously result-url)))
+      (let ((data (with-current-buffer buffer
+                    (potato--json-parse-result-buffer))))
+        (let ((kill-buffer-query-functions nil))
+          (kill-buffer buffer))
+        data))))
+
+(defun potato--read-input-line (start end)
+  (let ((uid-refs (loop for overlay in (overlays-in start end)
+                        for uid = (overlay-get overlay 'potato-user-ref)
+                        when uid
+                        collect (list (overlay-start overlay) (overlay-end overlay) uid overlay))))
+    (with-output-to-string
+      (loop with p = start
+            for uid-ref in (sort uid-refs (lambda (a b) (< (first a) (first b))))
+            if (< p (first uid-ref))
+            do (princ (buffer-substring p (first uid-ref)))
+            do (progn
+                 (princ (format "\U000f0001user:%s:%s\U000f0001"
+                                (third uid-ref) (buffer-substring (first uid-ref) (second uid-ref))))
+                 (setq p (second uid-ref))
+                 (delete-overlay (fourth uid-ref)))
+            finally (when (< p end)
+                      (princ (buffer-substring p end)))))))
+
+(defun potato-send-input-line ()
+  (interactive)
+  (let ((text (string-trim (potato--read-input-line potato--input-marker (point-max)))))
+    (when (not (equal text ""))
+      (delete-region potato--input-marker (point-max))
+      (when potato--input-function
+        (funcall potato--input-function text)))))
+
+(defvar potato-channel-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "RET") 'potato-send-input-line)
+    (define-key map (kbd "@") 'potato-insert-user)
+    map))
+
+(defun potato--insert-message (message-id timestamp from text)
+  (save-excursion
+    (goto-char potato--output-marker)
+    (let ((new-pos (loop with prev-pos = (point)
+                         for pos = (previous-single-char-property-change prev-pos 'potato-timestamp)
+                         until (let ((prop (get-char-property pos 'potato-timestamp)))
+                                 (and prop (string< prop timestamp)))
+                         do (setq prev-pos pos)
+                         until (= pos (point-min))
+                         finally
+                         return prev-pos)))
+      (goto-char new-pos)
+      (let ((inhibit-read-only t))
+        (insert (propertize (concat (propertize (format "[%s] " from)
+                                                'face 'potato-message-from)
+                                    text
+                                    "\n")
+                            'read-only t
+                            'potato-message-id message-id
+                            'potato-timestamp timestamp
+                            'front-sticky '(read-only)))))))
+
+(define-derived-mode potato-channel-mode nil "Potato"
+  "Mode for Potato channel content"
+  (use-local-map potato-channel-mode-map)
+  (setq-local potato--output-marker (make-marker))
+  (setq-local potato--input-marker (make-marker))
+  (set-marker potato--output-marker (point-max))
+  (setq-local potato--input-function nil)
+  (insert "channel> ")
+  (add-text-properties (point-at-bol) (point)
+                       (list 'read-only t
+                             'rear-nonsticky t
+                             'front-sticky '(read-only)
+                             'inhibit-line-move-field-capture t
+                             'field 'output))
+  (set-marker-insertion-type potato--output-marker t)
+  (set-marker potato--input-marker (point-max)))
+
+(cl-defun debug-insert-message (&optional (time 0))
+  (let ((timestamp (format "20150101T12:00:%02dZ" time))
+        (message-id (format "msgid%d" time)))
+    (potato--insert-message message-id timestamp "Palle" "Hello test message here")))
+
+(defun potato--after-user-change-modification (start end &rest ignore)
+  (let ((inhibit-modification-hooks t))
+    (let ((real-start (max 1 (1- start)))
+          (real-end   (min (1+ (buffer-size)) (1+ end)))
+          (any-change nil))
+      (save-excursion
+        nil))))
+
+(defun potato-insert-user ()
+  "Select a username to be inserted into the new message."
+  (interactive)
+  (let ((result (let ((completion-ignore-case t))
+                  (completing-read "User: " (mapcar #'second potato--users) nil t nil nil nil t))))
+    (if (equal result "")
+        ;; Blank string, simply insert an @-sign
+        (insert "@")
+      ;; ELSE: Insert a username tag
+      (let ((element (find result potato--users :key #'second :test #'equal)))
+        (unless element
+          (error "Selected element not found in user list: %S" element))
+        (let ((start (point)))
+          (insert (second element))
+          (let ((overlay (make-overlay start (point) nil nil nil)))
+            (overlay-put overlay 'face 'potato-message-input-user-name)
+            (overlay-put overlay 'potato-user-ref (first element))))))))
 
 (defun potato--input (str)
   (let ((url-request-data (encode-coding-string (json-encode `((text . ,str))) 'utf-8)))
@@ -115,7 +242,8 @@
   (let ((addr (potato--assoc-with-check 'addr element))
         (description (potato--assoc-with-check 'description element)))
     (propertize description
-                'mouse-face 'hilight
+                'font-lock-face 'link
+                'mouse-face 'highlight
                 'help-echo (format "mouse-2: browse url: %s" addr))))
 
 (defun potato--parse-json-decode-element (element)
@@ -260,9 +388,45 @@
                      (potato--create-buffer name cid))))
     buffer))
 
-(defun potato-client ()
-  (interactive)
-  (let ((buffer (potato--find-channel-buffer potato-channel-id)))
+(defun potato--request-channel-list ()
+  (let ((result (potato--url-retrieve-synchronous "/channels" "GET" :use-global t)))
+    (let ((channels (loop
+                     for domain across result
+                     for domain-id = (potato--assoc-with-check 'id domain)
+                     for domain-name = (potato--assoc-with-check 'name domain)
+                     append (loop
+                             for group across (potato--assoc-with-check 'groups domain)
+                             for group-id = (potato--assoc-with-check 'id group)
+                             for group-name = (potato--assoc-with-check 'name group)
+                             append (loop
+                                     for channel across (potato--assoc-with-check 'channels group)
+                                     for channel-id = (potato--assoc-with-check 'id channel)
+                                     for channel-name = (potato--assoc-with-check 'name channel)
+                                     collect (list channel-id domain-name group-name channel-name))))))
+      channels)))
+
+(defun potato--choose-channel-id ()
+  (let* ((channels (coerce (potato--request-channel-list) 'vector))
+         (names-list (loop for i from 0 below (length channels)
+                           for chan = (aref channels i)
+                           for channel-name = (fourth chan)
+                           unless (or (string-match "^Private channel for users" channel-name))
+                           collect (cons (if (loop for i2 from 0 below (length channels)
+                                                   when (and (/= i i2) (equal (fourth (aref channels i2)) channel-name))
+                                                   return t
+                                                   finally (return nil))
+                                             (format "%s (%s/%s)" channel-name (second chan) (third chan))
+                                           channel-name)
+                                         (first chan)))))
+    (let ((result (completing-read "Channel: " names-list nil t nil nil nil nil)))
+      (let ((e (find result names-list :key #'car :test #'equal)))
+        (unless e
+          (error "Did not find selected channel"))
+        (cdr e)))))
+
+(defun potato-client (channel-id)
+  (interactive (list (potato--choose-channel-id)))
+  (let ((buffer (potato--find-channel-buffer channel-id)))
     (switch-to-buffer buffer)))
 
 (provide 'potato)
